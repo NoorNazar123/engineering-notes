b7b6bcf02eb4b0ab7cdd3b60433e41f7e29eaed5
M Noor Nazar <noor.nazar.programmer@gmail.com>
2026-09-23 17:50:03 +0500

    allow production frontend cors

--- mini-crud-idea/main.py
+++ mini-crud-idea/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://eastern-illa-noornazar-34db511b.koyeb.app",
+        "https://engineering-notes-ten.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
