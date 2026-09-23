97f07a629ce2344fd22664790e7b9928e01fcf96
M Noor Nazar <noor.nazar.programmer@gmail.com>
2026-09-23 17:36:25 +0500

    update frontend production config

--- mini-crud-idea/main.py
+++ mini-crud-idea/main.py
@@ -26,12 +26,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://eastern-illa-noornazar-34db511b.koyeb.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 Base.metadata.create_all(bind=engine)
 
